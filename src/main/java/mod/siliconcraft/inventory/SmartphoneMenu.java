8c0693a093772875731276da03a6e95e17886046
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-03-04 19:09:51 +0900

    Smartphone menu wip

---
+++ src/main/java/mod/siliconcraft/inventory/SmartphoneMenu.java
@@ -0,0 +1,21 @@
+package mod.siliconcraft.inventory;
+
+import net.minecraft.client.gui.chat.NarratorChatListener;
+import net.minecraft.client.gui.screens.Screen;
+
+public class SmartphoneMenu extends Screen {
+
+    protected SmartphoneMenu() {
+        super(NarratorChatListener.NO_TITLE);
+    }
+
+    @Override
+    protected void init() {
+        
+    }
+
+    private void createGlobalControls() {
+        
+    }
+    
+}
